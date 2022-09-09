6c5ef115d4526e300f264896a64acd2925676921
rownokbosunia <rownokbosunia.dev@gmail.com>
2022-09-09 13:42:27 +0600

    react typscript and electron template

---
+++ public/main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path');
+const isDev = require('electron-is-dev');
+require('@electron/remote/main').initialize()
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      enableRemoteModule : true
+    }
+  })
+
+  win.loadURL(isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`)
+  
+  if (isDev) {
+    win.webContents.openDevTools({ mode: 'detach' });
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
